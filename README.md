0903845b858083cb312fe8e9f07e41d334355cd8
Daniel Kossmann <hello@danielkossmann.com>
2025-05-10 13:50:14 -0300

    Add local-wp-reset.sh script and update README.md with usage instructions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wp-tools
 The tools I use to help with some WordPress-related tasks.
+
+## local-wp-reset.sh
+
+Quickly reset WordPress and still keep themes plus some plugins, with optional `--language` parameter:
+
+```
+local-wp-reset-v2.sh --language=pt_BR
+```
+
+Requires: WP-CLI
---
+++ local-wp-reset.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+# Configuration variables
+SITE_URL="test.local"
+ADMIN_USER="kossmann"
+ADMIN_PASSWORD="kossmann"
+ADMIN_EMAIL="dev-email@dev-domain.local"
+THEME="twentytwentyfour"
+LANGUAGE="en_US"
+
+# Parse command line arguments
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --language=*)
+            LANGUAGE="${1#*=}"
+            ;;
+    esac
+    shift
+done
+
+# Delete plugins, but keep the ones I need: starting with dk-, auto-login and index.php
+find wp-content/plugins/ -mindepth 1 -maxdepth 1 -type d ! -name 'dk-*' ! -name 'auto-login' ! -name 'index.php' -exec trash {} +;
+
+# Delete everything in wp-content except plugins/ and themes/
+find wp-content/ -mindepth 1 -maxdepth 1 -type d ! -name 'plugins' ! -name 'themes' -exec trash {} +;
+
+# Reset database
+wp db reset --yes;
+
+# Install WordPress
+wp core install --url="$SITE_URL" --title="" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL";
+
+# Activate all plugins
+wp plugin activate --all;
+
+# Activated desired theme
+wp theme activate ${THEME};
+
+# Set language, useful for testing internationalization
+wp language core install ${LANGUAGE} --activate;
